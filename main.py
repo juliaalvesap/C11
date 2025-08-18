6131bb5c567c9bcd243b5f72ba9da5e0a30f5648
juliaalvesap <alvesjulia072072@gmail.com>
2025-08-18 20:59:36 -0300

    criando conflito

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import numpy as np
 
 def main():
-    participantes = ["Ana", "Bruno", "Carla", "Daniel", "Elisa"]
+    participantes = ["Anna", "Bianca", "Julia", "Mavi", "Mary"]
 
     # Sorteia um índice válido na lista
     indice_sorteado = np.random.randint(0, len(participantes))
 
     # Usa o índice para pegar o vencedor
-    vencedor = participantes[indice_sorteado]
+    vencedora = participantes[indice_sorteado]
 
     print(f"Número sorteado: {indice_sorteado}")
-    print(f"O vencedor do sorteio é: {vencedor}")
+    print(f"A vencedora do sorteio é: {vencedora}")
 
     # Gera o arquivo de texto com o resultado
     with open("resultado.txt", "w", encoding="utf-8") as f:
         f.write(f"Número sorteado: {indice_sorteado}\n")
-        f.write(f"O vencedor do sorteio é: {vencedor}\n")
+        f.write(f"A vencedora do sorteio é: {vencedora}\n")
 
 if __name__ == "__main__":
     main()
